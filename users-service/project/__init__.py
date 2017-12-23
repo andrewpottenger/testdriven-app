26328da9f0d1bc55b8e95539f32db4c3a849b763
Andrew Pottenger <apotts15@gmail.com>
2017-12-22 19:24:24 -0600

    Add React functional list component and CORS support

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -2,6 +2,7 @@
 
 import os
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the db
@@ -12,6 +13,9 @@ def create_app():
 
     # instantiate the app
     app = Flask(__name__)
+
+    # enable CORS
+    CORS(app)
 
     # set config
     app_settings = os.getenv('APP_SETTINGS')
