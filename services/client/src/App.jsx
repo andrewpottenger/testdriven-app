b38f1b128f561a04bd512ca80f98a0bba5a668c0
Andrew Pottenger <apotts15@gmail.com>
2017-12-30 19:44:28 -0600

    Add login, logout and registration client functionality

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import axios from 'axios';
 
 import NavBar from './components/NavBar';
+import Logout from './components/Logout';
 import Form from './components/Form';
 import About from './components/About';
 import UsersList from './components/UsersList';
@@ -20,10 +21,14 @@ class App extends Component {
                 username: '',
                 email: '',
                 password: ''
-            }
+            },
+            isAuthenticated: false,
         };
         this.addUser = this.addUser.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+        this.handleFormChange = this.handleFormChange.bind(this);
+        this.logoutUser = this.logoutUser.bind(this);
     };
     componentDidMount() {
         this.getUsers();
@@ -51,6 +56,40 @@ class App extends Component {
         const obj = {};
         obj[event.target.name] = event.target.value;
         this.setState(obj);
+    };
+    handleUserFormSubmit(event) {
+        event.preventDefault();
+        const formType = window.location.href.split('/').reverse()[0];
+        let data = {
+            email: this.state.formData.email,
+            password: this.state.formData.password,
+        };
+        if (formType === 'register') {
+            data.username = this.state.formData.email;
+        }
+        const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
+        axios.post(url, data)
+        .then((res) => {
+            this.setState({
+                formData: {username: '', email: '', password: '' },
+                username: '',
+                email: '',
+                isAuthenticated: true,
+            });
+            window.localStorage.setItem('authToken', res.data.auth_token);
+            this.getUsers()
+        })
+        .catch((err) => { console.log(err); });
+    };
+    handleFormChange(event) {
+        const obj = this.state.formData;
+        obj[event.target.name] = event.target.value;
+        console.log(this.state.formData);
+        this.setState(obj);
+    };
+    logoutUser() {
+        window.localStorage.clear();
+        this.setState({ isAuthenticated: false });
     };
     render() {
         return (
@@ -81,12 +120,24 @@ class App extends Component {
                                     <Form
                                         formType={'Register'}
                                         formData={this.state.formData}
+                                        handleUserFormSubmit={this.handleUserFormSubmit}
+                                        handleFormChange={this.handleFormChange}
+                                        isAuthenticated={this.state.isAuthenticated}
                                     />
                                 )} />
                                 <Route exact path='/login' render={() => (
                                     <Form
                                         formType={'Login'}
                                         formData={this.state.formData}
+                                        handleUserFormSubmit={this.handleUserFormSubmit}
+                                        handleFormChange={this.handleFormChange}
+                                        isAuthenticated={this.state.isAuthenticated}
+                                    />
+                                )} />
+                                <Route exact path='/logout' render={() => (
+                                    <Logout
+                                        logoutUser={this.logoutUser}
+                                        isAuthenticated={this.state.isAuthenticated}
                                     />
                                 )} />
                             </Switch>
