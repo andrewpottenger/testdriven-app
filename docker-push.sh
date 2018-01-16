19adb28d7d65d8698baab87596f4c2303752147a
Andrew Pottenger <apotts15@gmail.com>
2018-01-15 20:06:30 -0600

    Updated docker environment to be dynamica based on the travis brnach. Fixed the docker push script

--- docker-push.sh
+++ docker-push.sh
@@ -2,16 +2,18 @@
 
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
-    if [ "$TRAVIS_BRANCH" == "staging" ] ||  [ "$TRAVIS_BRANCH" == "production" ]
+
+    if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ]
     then
         curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+
         unzip awscli-bundle.zip
         ./awscli-bundle/install -b ~/bin/aws
         export PATH=~/bin:$PATH
         # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
-        eval $(aws ecr get-login --region us-east-1 --no-include-email)
+        eval $(aws ecr get-login --region us-west-2 --no-include-email)
         export TAG=$TRAVIS_BRANCH
-        export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
+        export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
     fi
 
     if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ]
@@ -25,8 +27,7 @@ then
         docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
         docker push $REPO/$USERS_DB:$TAG
         # client
-        docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REA
-        CT_APP_USERS_SERVICE_URL=TBD
+        docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=TBD
         docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
         docker push $REPO/$CLIENT:$TAG
         # swagger
